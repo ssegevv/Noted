aa1e2ff4521cb535e04e1c46a76cb562723b3dc4
ssegevv <segevb97@gmail.com>
2025-03-28 14:48:55 +0300

    react in vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import react ,{ useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <p className='text-3xl font-bold underline"'>hello</p>
+    </div>
+  )
+}
+
+export default App
